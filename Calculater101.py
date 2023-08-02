8bec6c67bef32f0518de9732b38e032261c98f5d
anja-bruwer <anjabruwer@gmail.com>
2023-08-02 10:48:26 +0200

    New changes

--- Calculater101.py
+++ Calculater101.py
@@ -9,38 +9,38 @@ while True:
    print("5. Exit")
    ###diffferent options that can be used for caluculations
 
-   print("\nEnter Your Choice (1-5): ", end="")
-   ###inout the option chosen
+   print("\nEnter Your Choice (1-5): ")
+   ###input the option chosen
 
-   ch = int(input())
+   answ = int(input())
 
-   if ch>=1 and ch<=4:
-      print("\nEnter Two Numbers: ", end="")
+   if answ>=1 and answ<=4:
+      print("\nEnter Two Numbers: ")
       numOne = float(input())
       numTwo = float(input())
       ### if on of the option is chosen ask for the two numbers for the calculations 
 
-   if ch==1:
+   if answ==1:
       res = numOne + numTwo
       print("\nResult =", res)
       ### if addition is chosen the two numbers will be + together 
 
-   elif ch==2:
+   elif answ==2:
       res = numOne - numTwo
       print("\nResult =", res)
       ### if subtraction is chosen the two numbers will be - together 
 
-   elif ch==3:
+   elif answ==3:
       res = numOne * numTwo
       print("\nResult =", res)
       ### if multiplution is chosen the two numbers will be x together 
 
-   elif ch==4:
+   elif answ==4:
       res = numOne / numTwo
       print("\nResult =", res)
       ### if division is chosen the two numbers will be / 
 
-   elif ch==5:
+   elif answ==5:
       break
         ### if exit is chose the program will end
 
@@ -48,6 +48,7 @@ while True:
       print("\nInvalid Input!..Try Again!")
       ## if unvalid option is chosen error message will show
 
+   print("\n---Next Calculation---")
    print("------------------------")  
    ### shows start of next calculation
 
